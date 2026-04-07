d1c9c7d6638b28c84b420323835713da54725df8
hieuka001 <127222971+hieuka001@users.noreply.github.com>
2026-04-07 23:45:01 +0700

    Update music-content.js

--- music-content.js
+++ music-content.js
@@ -1,6 +1,6 @@
 window.WEDDING_MUSIC = {
   // Doi link nhac tai day (mp3)
-  src: "https://github.com/hieuka001/lecuoi/raw/refs/heads/main/leduong.mp3",
+  src: "https://cdn.jsdelivr.net/gh/hieuka001/thiepcuoitinnhi@main/50nam.mp3",
   // am luong tu 0 -> 1
   volume: 0.75
 };
