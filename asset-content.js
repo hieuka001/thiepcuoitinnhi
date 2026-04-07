1587cfd73a9a01bac06864154468b1db416c6767
hieuka001 <127222971+hieuka001@users.noreply.github.com>
2026-04-07 23:48:01 +0700

    Update transferQr image URL in asset-content.js

--- asset-content.js
+++ asset-content.js
@@ -1,5 +1,5 @@
 window.WEDDING_ASSETS = {
   // Cac anh phu tro khong phai anh cuoi
   giftBox: "https://pngimg.com/d/gift_PNG100271.png",
-  transferQr: "https://api.qrserver.com/v1/create-qr-code/?size=280x280&data=MB|0999999999|MAI%20THU|CHUC%20MUNG%20HANH%20PHUC"
+  transferQr: "https://raw.githubusercontent.com/hieuka001/Anki-images/main/uploads/temp_upload_20260407_222734.png?size=280x280&data=MB|CHUC%20MUNG%20HANH%20PHUC"
 };
